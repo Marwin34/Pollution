9611717fe34f6b5bbc43a810357feae3cf351787
Marwin34 <marcinwojsik@wp.pl>
2019-05-01 23:18:32 +0200

    Even more tests :(.

---
+++ src/pollution_test.erl
@@ -0,0 +1,15 @@
+%%%-------------------------------------------------------------------
+%%% @author Marcin
+%%% @copyright (C) 2019, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 01. May 2019 11:18 PM
+%%%-------------------------------------------------------------------
+-module(pollution_test).
+-author("Marcin").
+
+-include_lib("eunit/include/eunit.hrl").
+
+simple_test() ->
+  ?assert(true).
